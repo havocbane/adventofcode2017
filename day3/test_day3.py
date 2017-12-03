1f1cf96c8a8567edd28411b55d987af78c33b320
Joseph Bane <jbane@acm.org>
2017-12-03 17:41:17 -0500

    Add day 3 part 2 tests.

--- day3/test_day3.py
+++ day3/test_day3.py
@@ -1,6 +1,7 @@
 import pytest
 
 from day3 import spiral_memory
+from day3_part2 import spiral_memory as spiral_memory2
 
 
 @pytest.mark.parametrize('test_case,expected', [
@@ -12,4 +13,17 @@ from day3 import spiral_memory
 ])
 def test_spiral_memory(test_case, expected):
     actual = spiral_memory(test_case)
+    assert expected == actual
+
+
+@pytest.mark.parametrize('test_case,expected', [
+    (1, 1,),
+    (2, 2,),
+    (4, 4,),
+    (45, 54),
+    (148, 304,),
+    (777, 806,),
+])
+def test_spiral_memory2(test_case, expected):
+    actual = spiral_memory2(test_case)
     assert expected == actual
