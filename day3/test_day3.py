d1f0e39cf47c58f7f458ff3de8913d357d6df889
Joseph Bane <jbane@acm.org>
2017-12-03 16:00:04 -0500

    Fix whitespace in test file.

--- day3/test_day3.py
+++ day3/test_day3.py
@@ -4,12 +4,12 @@ from day3 import spiral_memory
 
 
 @pytest.mark.parametrize('test_case,expected', [
-	(1, 0,),
-	(12, 3,),
-	(23, 2,),
+    (1, 0,),
+    (12, 3,),
+    (23, 2,),
     (49, 6),
-	(1024, 31,),
+    (1024, 31,),
 ])
 def test_spiral_memory(test_case, expected):
-	actual = spiral_memory(test_case)
-	assert expected == actual
+    actual = spiral_memory(test_case)
+    assert expected == actual
