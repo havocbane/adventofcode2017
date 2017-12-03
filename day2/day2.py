4a0665ac69638475fbe3383b1532c76de56b495e
Joseph Bane <jbane@acm.org>
2017-12-02 20:08:17 -0500

    Add day 2 part 2 solution.

--- day2/day2.py
+++ day2/day2.py
@@ -17,11 +17,38 @@ def checksum(data):
     return result
 
 
+def checksum_evenly_divided(data):
+    lines = data.split('\n')
+    result = 0
+    for row in lines:
+        row = row.strip()
+        if not row:
+            continue
+        row_vals = [int(el) for el in row.split()]
+        result += compare_row(row_vals)
+    return result
+
+
+def compare_row(row_vals):
+    for x in row_vals:
+        for y in row_vals:
+            if x == y:
+                continue
+            if x > y:
+                numer, denom = x, y
+            else:
+                numer, denom = y, x
+            if numer % denom == 0:
+                return int(numer / denom)
+    raise Exception("uh oh!")
+
+
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         dat = f.read()
-    start = time.time()
-    result = checksum(dat)
-    end = time.time()
-    elapsed = end - start
-    print("Result: {0}, time {1}".format(result, elapsed))
+    for fn in (checksum, checksum_evenly_divided,):
+        start = time.time()
+        result = fn(dat)
+        end = time.time()
+        elapsed = end - start
+        print("Result: {0}, time {1}".format(result, elapsed))
--- day2/test_day2.py
+++ day2/test_day2.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 
-from day2 import checksum
+import pytest
+
+from day2 import checksum, checksum_evenly_divided
 
 
-def test_checksum():
+def test_checksum(benchmark):
     data = r"""5 1 9 5
 7 5 3
 2 4 6 8
 """
     expected = 18
-    actual = checksum(data)
+    actual = benchmark(checksum, data)
+    assert expected == actual
+
+
+def test_checksum_evenly_divided(benchmark):
+    data = r"""5 9 2 8
+9 4 7 3
+3 8 6 5"""
+    expected = 9
+    actual = benchmark(checksum_evenly_divided, data)
     assert expected == actual
