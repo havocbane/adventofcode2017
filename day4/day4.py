33621992586bdd986f409be41803fb44f0525738
Joseph Bane <jbane@acm.org>
2017-12-04 09:38:38 -0500

    Add day 4 part 2 solution.

--- day4/day4.py
+++ day4/day4.py
@@ -14,11 +14,26 @@ def valid_passphrases(data):
     return num_valid
 
 
+def valid_passphrases_anagrams(data):
+    num_valid = 0
+    for line in data.split('\n'):
+        words = line.split()
+        if not words:
+            continue
+        sorted_words = []
+        for word in words:
+            sorted_words.append(''.join(sorted(word)))
+        if len(words) == len(set(sorted_words)):
+            num_valid += 1
+    return num_valid
+
+
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         data = f.read()
-    start = time.time()
-    num = valid_passphrases(data)
-    end = time.time()
-    elapsed = end - start
-    print('Result: {0}, time: {1}'.format(num, elapsed))
+    for fn in (valid_passphrases, valid_passphrases_anagrams,):
+        start = time.time()
+        num = fn(data)
+        end = time.time()
+        elapsed = end - start
+        print('Result: {0}, time: {1}'.format(num, elapsed))
--- day4/test_day4.py
+++ day4/test_day4.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from day4 import valid_passphrases
+from day4 import valid_passphrases, valid_passphrases_anagrams
 
 
 def test_passphrases(benchmark):
@@ -11,4 +11,15 @@ aa bb cc dd aa
 aa bb cc dd aaa"""
     expected = 2
     actual = benchmark(valid_passphrases, data)
+    assert expected == actual
+
+
+def test_passphrases_with_anagrams(benchmark):
+    data = r"""abcde fghij
+abcde xyz ecdab
+a ab abc abd abf abj
+iiii oiii ooii oooi oooo
+oiii ioii iioi iiio"""
+    expected = 3
+    actual = benchmark(valid_passphrases_anagrams, data)
     assert expected == actual
