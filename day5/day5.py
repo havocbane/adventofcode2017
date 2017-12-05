aacb765f55755e441cc512febff0d1bd7743bad0
Joseph Bane <jbane@acm.org>
2017-12-05 06:02:26 -0500

    Add day 5 part 2 solution.

--- day5/day5.py
+++ day5/day5.py
@@ -15,6 +15,21 @@ def exit_maze(instructions):
     return count
 
 
+def exit_maze_with_check(instructions):
+    memory = [int(val) for val in instructions.split('\n')]
+    i = 0
+    count = 0
+    while i < len(memory):
+        step = memory[i]
+        if step >= 3:
+            memory[i] -= 1
+        else:
+            memory[i] += 1
+        i += step
+        count += 1
+    return count
+
+
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         data = f.read()
--- day5/test_day5.py
+++ day5/test_day5.py
@@ -2,11 +2,14 @@
 
 import pytest
 
-from day5 import exit_maze
+from day5 import exit_maze, exit_maze_with_check
 
 
-def test_exit_maze(benchmark):
+@pytest.mark.parametrize('fn,expected', [
+    (exit_maze, 5,),
+    (exit_maze_with_check, 10),
+])
+def test_exit_maze(benchmark, fn, expected):
     instructions = "0\n3\n0\n1\n-3"
-    expected = 5
-    actual = benchmark(exit_maze, instructions)
+    actual = benchmark(fn, instructions)
     assert expected == actual
