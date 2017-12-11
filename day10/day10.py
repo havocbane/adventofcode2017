abdb196aee340795919e471c695cbd612dafc360
Joseph Bane <jbane@acm.org>
2017-12-10 19:37:35 -0500

    Add day 10 part 2 solution.

--- day10/day10.py
+++ day10/day10.py
@@ -1,34 +1,57 @@
 #!/usr/bin/env python
 
+from functools import reduce
+import operator
 import time
 
 
-def knot_hash(input_lengths, list_size=256):
-    skip = 0
-    cur_pos = 0
-    l = list(range(list_size))
-    lengths = [int(el.strip()) for el in input_lengths.split(',')]
-    for length in lengths:
+def knot_hash(lengths, list_size=256, cur_pos=0, skip=0, cur_list=None):
+    if cur_list is None:
+        cur_list = list(range(list_size))
+    for k, length in enumerate(lengths):
         if length > list_size:
             raise Exception('uh oh!')
         if length > 0:
             tmp = []
             for i in range(length):
                 j = (cur_pos + i) % list_size
-                tmp.append(l[j])
+                tmp.append(cur_list[j])
             tmp.reverse()
             for i, el in enumerate(tmp):
                 j = (cur_pos + i) % list_size
-                l[j] = el
-        cur_pos = (cur_pos + length + skip) % list_size
-        skip += 1
-    return l[0] * l[1]
+                cur_list[j] = el
+        if (k < list_size):
+            cur_pos = (cur_pos + length + skip) % list_size
+            skip += 1
+    return cur_list[0] * cur_list[1], cur_list, cur_pos, skip
+
+def hash(ascii):
+    lengths = [ord(ch) for ch in ascii]
+    lengths += [17, 31, 73, 47, 23]
+    cur_pos = 0
+    skip = 0
+    sparse_hash = None
+    for _ in range(64):
+        _, sparse_hash, cur_pos, skip = knot_hash(lengths, cur_pos=cur_pos, skip=skip, cur_list=sparse_hash)
+    dense_hash = []
+    for i in range(0, len(sparse_hash), 16):
+        dense_hash.append(reduce(operator.xor, sparse_hash[i:i + 16]))
+    return ''.join(['{:02X}'.format(el) for el in dense_hash]).lower()
 
 
 if __name__ == '__main__':
     puzzle_input = '192, 69, 168, 160, 78, 1, 166, 28, 0, 83, 198, 2, 254, 255, 41, 12'
+
     start = time.time()
-    result = knot_hash(puzzle_input)
+    int_lengths = [int(el.strip()) for el in puzzle_input.split(',')]
+    result = knot_hash(int_lengths)[0]
+    end = time.time()
+    elapsed = end - start
+    print('Result: {result}, time: {elapsed}'.format(result=result, elapsed=elapsed))
+
+    start = time.time()
+    remove_whitespace = ''.join([ch.strip() for ch in puzzle_input.split()])
+    result = hash(remove_whitespace)
     end = time.time()
     elapsed = end - start
     print('Result: {result}, time: {elapsed}'.format(result=result, elapsed=elapsed))
--- day10/test_day10.py
+++ day10/test_day10.py
@@ -2,12 +2,22 @@
 
 import pytest
 
-from day10 import knot_hash
+from day10 import knot_hash, hash
 
 
-def test_knot_hash():#benchmark):
-    test_case = '3, 4, 1, 5'  # input lengths
+def test_knot_hash(benchmark):
+    test_case = [int(el.strip()) for el in '3, 4, 1, 5'.split(',')]  # input lengths
     expected = 12
-    #actual = benchmark(knot_hash, test_case, 5)
-    actual = knot_hash(test_case, 5)
+    actual = benchmark(knot_hash, test_case, list_size=5)[0]
+    assert expected == actual
+
+
+@pytest.mark.parametrize('test_case,expected', [
+    ('', 'a2582a3a0e66e6e86e3812dcb672a272',),
+    ('AoC 2017', '33efeb34ea91902bb2f59c9920caa6cd',),
+    ('1,2,3', '3efbe78a8d82f29979031a4aa0b16a9d',),
+    ('1,2,4', '63960835bcdc130f0b66d7ff4f6a5a8e',),
+])
+def test_hash(benchmark, test_case, expected):
+    actual = benchmark(hash, test_case)
     assert expected == actual
