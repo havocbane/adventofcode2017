ccdab1f9554c4e508bbd572fd74044023950a21c
Joseph Bane <jbane@acm.org>
2017-12-11 21:58:34 -0500

    Add day 11 part 1 solution.

---
+++ day11/day11.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+class Point(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+def find_z(x, y):
+    # x + y + z = 0
+    return 0 - x - y
+
+def distance(p1, p2):
+    return max(abs(p2.x - p1.x), abs(p2.y - p1.y), abs(p2.z - p1.z))
+
+fn = {
+    'n':  lambda x, y: (x - 1, y + 1,),
+    'ne': lambda x, y: (x,     y + 1,),
+    'se': lambda x, y: (x + 1, y,),
+    's':  lambda x, y: (x + 1, y - 1,),
+    'sw': lambda x, y: (x,     y - 1,),
+    'nw': lambda x, y: (x - 1, y,),
+}
+
+def shortest_path(walk):
+    p1 = Point(0, 0, 0)
+    p2 = Point(0, 0, 0)
+    for direction in walk:
+        p2.x, p2.y = fn[direction](p2.x, p2.y)
+        p2.z = find_z(p2.x, p2.y)
+    return distance(p2, p1)
+
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+        data = f.read()
+    result = shortest_path(data.strip().split(','))
+    print(result)
---
+++ day11/test_day11.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import pytest
+
+from day11 import shortest_path
+
+
+@pytest.mark.parametrize('test_case,expected', [
+    ('ne,ne,ne', 3,),
+    ('ne,ne,sw,sw', 0,),
+    ('ne,ne,s,s', 2,),
+    ('se,sw,se,sw,sw', 3,),
+])
+def test_shortest_path(benchmark, test_case, expected):
+    actual = benchmark(shortest_path, test_case.split(','))
+    assert expected == actual
