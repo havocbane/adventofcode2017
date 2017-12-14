390949deb40f5e49157898b5646698fd5d30eaaf
Joseph Bane <jbane@acm.org>
2017-12-13 23:00:12 -0500

    Add timing to day 11.

--- day11/day11.py
+++ day11/day11.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 
+import time
+
+
 class Point(object):
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
 
+
 def find_z(x, y):
     # x + y + z = 0
     return 0 - x - y
 
+
 def distance(p1, p2):
     return max(abs(p2.x - p1.x), abs(p2.y - p1.y), abs(p2.z - p1.z))
+
 
 fn = {
     'n':  lambda x, y: (x - 1, y + 1,),
@@ -21,6 +27,7 @@ fn = {
     'sw': lambda x, y: (x,     y - 1,),
     'nw': lambda x, y: (x - 1, y,),
 }
+
 
 def shortest_path(walk):
     p1 = Point(0, 0, 0)
@@ -36,5 +43,9 @@ def shortest_path(walk):
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         data = f.read()
+    start = time.time()
     shortest_path, furthest = shortest_path(data.strip().split(','))
-    print(shortest_path, furthest)
+    end = time.time()
+    elapsed = end - start
+    print('Result: {shortest_path}, {furthest}, time: {elapsed}'.format(
+          shortest_path=shortest_path, furthest=furthest, elapsed=elapsed))
